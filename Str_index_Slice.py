e3697678a71ca9eba53b81405f83c4c658b9a35f
Ajit Kumar <74779960+ajitnoogler@users.noreply.github.com>
2025-07-24 01:58:39 +0530

    Day 2 Uploads Functions

---
+++ Str_index_Slice.py
@@ -0,0 +1,11 @@
+a = "Welcome"
+
+print(a[0:7]) # Slice from index 0 up to (but not including) index 7
+print(a[1:6]) # Slice from index 1 to index 5
+print(a[:-2]) # means slice from start up to (but not including) the 2nd-last character
+print(a[-2:]) # means slice from 2nd-last character to the end
+
+
+# Index:     0   1   2   3   4   5   6
+# Letters:   W   e   l   c   o   m   e
+#           -7 -6  -5  -4  -3  -2  -1   ← Negative Indexing
